ebbadc8888d94f2a88a81d00d9194b923e0106bf
ardnmicc <micarabajal27@gmail.com>
2024-04-29 05:45:57 -0300

    API REST 1

---
+++ config.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const dbconnect = () => {
+    mongoose.set('strictQuery', true)
+    mongoose.connect("mongodb://127.0.0.1:27017/user_prueba", {} ,(err,res)=>{
+        if(!err){
+            console.log("Conexion correcta")
+        }else{
+            console.log("Error de conexion")
+        }
+    })
+}
+
+module.exports = dbconnect;
